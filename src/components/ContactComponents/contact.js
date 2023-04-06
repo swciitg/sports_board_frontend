83cd201c6f6d954a763ce595b5a3acc3c6377544
mohit <mohitsharma55337@gmail.com>
2023-04-06 18:02:18 +0530

    Contact Page Added

---
+++ src/components/ContactComponents/contact.js
@@ -0,0 +1,93 @@
+import React from 'react'
+import { SiLinkedin } from "react-icons/si";
+import { GrMail } from "react-icons/gr";
+import { IoMdCall } from "react-icons/io";
+import "./contact.css"
+import { BACKEND_URL } from '../../constants';
+const ContactComp1 = (props)=>{
+    return(
+        <>
+        <div className='Overall_Contact'>
+                <div className='Contact_block'>
+                    <div className='main_chair' >
+                        <div className='image_container'>
+                        <img className='main_image' src={BACKEND_URL+props.info.Image.url} alt=""/>
+                        </div>
+                    </div>
+                    <div className='flexing'>
+                            <div className='heading'>
+                                <p style={{margin:"10px"}}>{props.info.Position_Name}</p>
+                            </div>
+                            <div className='para'>
+                                    {props.info.Name} <br/>{props.info.Department}<br/><br/>"{props.info.Slogan}"<br/><br/> 
+                            </div>
+                            <div className='icons'>
+                                        <a href={"tel:"+props.info.Phone_Num} className='ico2'><IoMdCall size={50}/></a>
+                                        <a href={"mailto:"+props.info.Mail_ID} className='ico2'><GrMail size={50}/></a>
+                                        <a href={props.info.Linkedin_ID} className='ico2'><SiLinkedin size={50}/></a>
+                            </div>
+                    </div>
+                    </div>
+            </div>
+            <div className="smOverallContact">
+                    <div className="smContactBlock" >
+                        <div className='heading'>
+                                    <p style={{margin:"10px"}}>{props.info.Position_Name}</p>
+                                </div>
+                        </div>
+                        <div className='main_chair' >
+                            <div className='image_container'>
+                                <img className='main_image' src={BACKEND_URL+props.info.Image.url} alt=""/>
+                                <div className='para'>
+                                    {props.info.Name} <br/>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='desc'>
+                            <div className='para'>
+                                    "{props.info.Department}"
+                            </div>
+                        </div>
+                        <div>
+                            <div className='desc slogan'>
+                                "{props.info.Slogan}"<br/><br/> 
+                            </div>
+                        </div>
+                        <div className='icons'>
+                            <a href={"tel:"+props.info.Phone_Num} className='ico2'><IoMdCall size={50}/></a>
+                            <a href={"mailto:"+props.info.Mail_ID} className='ico2'><GrMail size={50}/></a>
+                            <a href={props.info.Linkedin_ID} className='ico2'><SiLinkedin size={50}/></a>
+                        </div>
+            </div>
+        </>
+ );
+}
+export default ContactComp1;
+
+{/* <div className='idhar'>
+<div className='main_chair' >
+<div className='main_image'>
+<img className='img2' src={BACKEND_URL+props.info.Image.url} alt=""/>
+</div>
+<div className='flexing'><div className='heading'><p style={{margin:"10px"}}>{props.info.Position_Name}</p></div>
+<div className='para'>
+        {props.info.Name} <br/>{props.info.Department}<br/><br/>"{props.info.Slogan}"<br/><br/>
+   <div className='icons'>
+   <a href={"tel:"+props.info.Phone_Num} className='ico2'><IoMdCall size={50}/></a>
+    <a href={"mailto:"+props.info.Mail_ID} className='ico2'><GrMail size={50}/></a>
+    <a href={props.info.Linkedin_ID} className='ico2'><SiLinkedin size={50}/></a>
+   
+    </div> 
+</div>
+
+
+</div>
+
+
+
+
+
+
+
+</div>
+</div> */}
--- src/screens/ContactScreen.js
+++ src/screens/ContactScreen.js
@@ -1,11 +1,31 @@
 import React from "react";
-
+import { useEffect, useState } from "react";
+import { fetchContactPageData } from "../api";
+import Navbar from "../components/Navbar/navbar";
+import Loader from "../components/Loader/Loader";
+import ContactComp1 from "../components/ContactComponents/contact";
+import Footer from "../components/Footer/Footer";
 const ContactScreen = () => {
-   return (
-        <>
-            Contact Screen
-        </>
-    );  
+    const [ data, setData]=useState([]);
+    const [isLoaded, setLoad] = useState(false);
+    useEffect(() => {
+        fetchContactPageData()
+           .then((res) => { setData(res.data); setLoad(true) })
+           .catch(setLoad(false))
+   }, []) 
+    console.log(data);
+    return (
+      isLoaded ? (<div>
+        <Navbar name={"Contact Us"} slogan={"We are here for you"}/>
+        {data.map((item,index)=>{
+          return index%2?<ContactComp1 info={item}/>:<ContactComp1 info={item}/>
+        })}
+        <Footer />
+      </div>) : 
+      (<Loader/>)
+      
+    )
+  }
 
-}
+  
 export default ContactScreen;
