ba00f64ddcc1707b534d8e40655159fa9ff3e4c9
Geetmanik <geetmanik2@gmail.com>
2023-04-08 07:24:59 +0530

    Footer code clean

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,12 +1,12 @@
 import React from "react";
-import "./footer.css";
+import "./Footer.css";
 import gymkhanalogo from "../../assets/Images/footer.png"
-import youTubelogo from "../../assets/Images/icona.png"
-import twitterlogo from "../../assets/Images/iconb.png"
-import instalogo from "../../assets/Images/iconc.png"
-import linkedInlogo from "../../assets/Images/icond.png"
-import SWCLogo from "../../assets/Images/swc_logo.png"
-import copyRightImage from "../../assets/Images/CopyRightImage.svg"
+import youTubelogo from "../../assets/Footer/YoutubeLogo.png"
+import twitterlogo from "../../assets/Footer/TwitterLogo.png"
+import instalogo from "../../assets/Footer/InstagramLogo.png"
+import linkedInlogo from "../../assets/Footer/LinkdinLogo.png"
+import SWCLogo from "../../assets/Footer/SwcLogo.png"
+import copyRightImage from "../../assets/Footer/CopyRightImage.svg"
 
 
 const Footer = (props) =>
@@ -18,36 +18,39 @@ const Footer = (props) =>
     else color = "#f4f4f4";
 
     return (
-        <div id="containerone"  className="flex flex-row gap-64 w-100% h-80 mt-0 pl-0 px-0" style={{position:'relative',zIndex:'1',backgroundColor: color}}>
-            <div id="part1" className="flex ml-44 gap-x-20 justify-around">
-                <div className="image w-52 h-52"><img id="img" className=" h-52 w-52" src={gymkhanalogo} alt=""/></div>
-                <div className="content flex flex-col flex-wrap items-start justify-start">
-                    <div id="part3" className="details flex flex-col flex-wrap items-start justify-start gap-1 p-1 m-1 ">
+        <div id="footer-container"  className="flex flex-row   " style={{backgroundColor: color}}>
+            <div id="students-ghymkhana-council" className="flex ">
+                <div className="ghymkhana-logo "><img  src={gymkhanalogo} alt=""/></div>
+                <div className="flex flex-col flex-wrap items-start justify-start">
+                    <div className="ghymkhana-details flex flex-col flex-wrap items-start justify-start gap-1  ">
                         <div id="heading">
-                            <h1 className="text-2xl font-extrabold " id="head">STUDENTS GYMKHANA</h1>
-                            <h1 className=" text-2xl   font-extrabold" id="head" >COUNCIL</h1>
+                            <h1 className="text-2xl font-extrabold ">STUDENTS GYMKHANA</h1>
+                            <h1 className=" text-2xl   font-extrabold">COUNCIL</h1>
                         </div>
-                        <p align="left" id="one" className="text-sm">OLD SAC Building IIT Guwahati,<br/> Guwahati,<span id="assam"> Assam - 781039</span></p>
-                        <p align="left" id="two" className="text-sm">+91 -361-258162<br/>sgc@iitg.ac.in</p>
+                        <div id="details">
+                        <p align="left"  className="text-sm">OLD SAC Building IIT Guwahati,<br/> Guwahati,<span > Assam - 781039</span></p>
+                        <p align="left"  className="text-sm">+91 -361-258162<br/>sgc@iitg.ac.in</p>
+                        </div>
+                        <div id="social-media-links" className="flex flex-row pt-3  ">
+                        <a href="https://www.youtube.com/@sportsboardiitguwahati"><img  className="h-8 w-8" src={youTubelogo} alt=""/></a>
+                        <a href="https://twitter.com/IITGuwahati"><img  className="h-8 w-8" src={twitterlogo} alt=""/></a>
+                        <a href="https://instagram.com/sports_iit_guwahati?igshid=YmMyMTA2M2Y="><img   className="h-8 w-8" src={instalogo} alt=""/></a>
+                        <a href="https://www.linkedin.com/school/iitg/"><img  className="h-8 w-8" src={linkedInlogo} alt="" /> </a>
                     </div>
-                    <div id="small" className="flex flex-row p-3 ml-4 mt-0 relative right-6 ">
-                        <a href="https://www.youtube.com/@sportsboardiitguwahati"><img id="img1" className="h-8 w-8" src={youTubelogo} alt=""/></a>
-                        <a href="https://twitter.com/IITGuwahati"><img id="img1" className="h-8 w-8" src={twitterlogo} alt=""/></a>
-                        <a href="https://instagram.com/sports_iit_guwahati?igshid=YmMyMTA2M2Y="><img id="img1"  className="h-8 w-8" src={instalogo} alt=""/></a>
-                        <a href="https://www.linkedin.com/school/iitg/"><img id="img1" className="h-8 w-8" src={linkedInlogo} alt="" /> </a>
                     </div>
+                
                 </div>
             </div>
-            <div id="part2" className=" flex flex-col mt-14  gap-2">
-                <div id="para" className="flex flex-row gap-2 ">
-                    <div className="img3">
-                        <img id="img3" className="h-16 w-16" src={SWCLogo} alt=""></img>
+            <div id="students-web-committee" className=" flex flex-col   gap-2">
+                <div id="swc-details" className="flex flex-row  ">
+                    <div>
+                        <img  className="h-16 w-16" src={SWCLogo} alt=""></img>
                     </div>
-                    <p align="left" id="three" className="text-sm content-start ">Maintained and Developed <span id="student">by Students</span><br/>Web committee<br/>IIT Guwahati</p>
+                    <div align="left"  className="text-sm  ">Maintained and Developed <span>by Students</span><br/>Web committee<br/>IIT Guwahati</div>
                 </div>
-                <div id="last" className="element2 flex flex-row">
-                    <img id="imglast" src={copyRightImage}/>
-                    <p id="four" className="text-sm">Students Gymkhana Council <span id="content-four"> Council IIT Guwahati</span></p>
+                <div id="copy-right" className="flex flex-row">
+                    <img  src={copyRightImage}/>
+                    <p  className="text-sm">Students Gymkhana Council <span id="content-four"> Council IIT Guwahati</span></p>
                 </div>
             </div>
         </div>
