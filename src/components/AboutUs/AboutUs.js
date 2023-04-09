af6510eeaf8123e5e60286cad2b01b83934549c7
Geetmanik <geetmanik2@gmail.com>
2023-04-09 20:18:28 +0530

    About page ui

--- src/components/AboutUs/AboutUs.js
+++ src/components/AboutUs/AboutUs.js
@@ -1,31 +1,22 @@
 import React from "react";
 import { BACKEND_URL } from "../../constants";
-import "./AboutUsStyle.css"
+import "./AboutUs.css"
 
 const AboutUS = (props) =>
 {
     return (
-        <div className="overall_about">
-            <div className="about_block">
-                <div className="containerabout1">
-                    <div>
-                        <img src={BACKEND_URL+props.media}className="imgabout" alt=""/>
-                    </div>
+        <div className="about-section">
+     
+                <div className="about-image">
+                 
+                        <img src={BACKEND_URL+props.media} alt=""/>
+              
                 </div>
-                <div className="containerabout2">
-                    <div className="head_about max-sm:text-3xl mx-10" style={{lineHeight:'123%'}}>{props.name}</div>
-                    <p className="para_about " style={{lineHeight:'160%'}}>{props.desc}</p>
+                <div className="about-details">
+                    <div className="about-heading max-sm:text-3xl " >{props.name}</div>
+                    <div className="about-desc " >{props.desc}</div>
                 </div>   
-            </div>
-            <div className="smabout_block">
-                <div className="head_about text-6xl max-sm:text-3xl">{props.name}</div>
-                <div style={{display:'flex',justifyContent:'center'}}>
-                    <img src={BACKEND_URL+props.media} className="imgabout" alt="" />
-                </div>
-                <div>
-                    <p className="para_about">{props.desc}</p>
-                </div>
-            </div>
+   
         </div>
     );
 }
