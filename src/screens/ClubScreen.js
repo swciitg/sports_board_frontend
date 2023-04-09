126b15e44df1ee1cda3164ec3b2b4f20c1835e22
mohit <mohitsharma55337@gmail.com>
2023-04-09 23:56:22 +0530

    each event page added

--- src/screens/ClubScreen.js
+++ src/screens/ClubScreen.js
@@ -1,10 +1,9 @@
 import React from "react";
-import { useLocation,useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { fetchClubData } from "../api";
 import Navbar from "../components/Navbar/navbar";
 import AboutUS from "../components/AboutUs/AboutUs";
-import ClubpageComp2 from "../components/ClubpageComponent/ClubpageComp2";
 import MediaComponent from "../components/MediaComponent/MediaComponent";
 import Footer from "../components/Footer/Footer";
 import Loader from "../components/Loader/Loader";
@@ -22,9 +21,9 @@ const ClubScreen = () => {
    let col1 =1;
    return (
     isLoaded ? (<div>
-        <Navbar name={data.ClubName + "CLUB"} slogan={data.ClubSlogan}/>
+        <Navbar name={data.ClubName + "CLUB"} slogan={data.ClubSlogan} carousel={[data.Background_Image]}/>
         {data.Components.map((item,index) =>{
-            return (index)%2===0?<AboutUS name={item.Component_name} desc={item.Component_data} media={item.Component_Image.url}/>:<ClubpageComp2 name={item.Component_name} desc={item.Component_data} media={item.Component_Image.url}/>
+            return <AboutUS name={item.Component_name} desc={item.Component_data} media={item.Component_Image.url}/>
         })}
         {data.Media_Components.map((item,index) =>{
             return <MediaComponent name={item.Component_name} desc={item.Media_Description} media={item.Images} />
--- src/screens/EventScreen.js
+++ src/screens/EventScreen.js
@@ -1,10 +1,36 @@
-import React from "react";
 
+import React from "react";
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { fetchClubData, fetchEventData } from "../api";
+import Navbar from "../components/Navbar/navbar";
+import AboutUS from "../components/AboutUs/AboutUs";
+import MediaComponent from "../components/MediaComponent/MediaComponent";
+import Footer from "../components/Footer/Footer";
+import Loader from "../components/Loader/Loader";
 const EventScreen = () => {
+    const [data, setData] = useState("");
+    const {id}=useParams()
+    const [isLoaded, setLoad] = useState(false);
+    useEffect(() => {
+        fetchEventData(id)
+           .then((res) => { setData(res.data[0]); setLoad(true); })
+           .catch(setLoad(false))
+   }, [])
+   console.log(data)
    return (
-        <>
-            Event Screen
-        </>
+    isLoaded ? (<div>
+        <Navbar name={data.Event_Name} slogan={data.Event_Slogan}   carousel={[data.Background_Image]}/>
+        {data.Components.map((item,index) =>{
+            return <AboutUS name={item.Component_name} desc={item.Component_data} media={item.Component_Image.url}/>
+        })}
+        {data.Media_Components?.map((item,index) =>{
+            return <MediaComponent name={item.Component_name} desc={item.Media_Description} media={item.Images} />
+        })}
+        
+        <Footer />
+       </div> ):
+       (<Loader/>) 
     );  
 
 }
