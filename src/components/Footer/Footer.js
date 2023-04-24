53c05207326c5fb50d5b4906fe14fa93bb32248a
mohit <mohitsharma55337@gmail.com>
2023-04-24 19:26:42 +0530

    footer icons color change added

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,14 @@
 import React from "react";
 import "./footer.css";
 import gymkhanalogo from "../../assets/Images/sports board final logo.png"
-import youTubelogo from "../../assets/Footer/YoutubeLogo.png"
-import twitterlogo from "../../assets/Footer/TwitterLogo.png"
-import instalogo from "../../assets/Footer/InstagramLogo.png"
-import linkedInlogo from "../../assets/Footer/LinkdinLogo.png"
+import youTubelogoB from "../../assets/Footer/YoutubeLogo_B.png"
+import youTubelogoW from "../../assets/Footer/YoutubeLogo_W.png"
+import twitterlogoB from "../../assets/Footer/TwitterLogo_B.png"
+import twitterlogoW from "../../assets/Footer/TwitterLogo_W.png"
+import instalogoB from "../../assets/Footer/InstagramLogo_B.png"
+import instalogoW from "../../assets/Footer/InstagramLogo_W.png"
+import linkedInlogoB from "../../assets/Footer/LinkdinLogo_B.png"
+import linkedInlogoW from "../../assets/Footer/LinkdinLogo_W.png"
 import SWCLogo from "../../assets/Footer/SwcLogo.png"
 import copyRightImage from "../../assets/Footer/CopyRightImage.svg"
 
@@ -13,10 +17,20 @@ const Footer = (props) =>
 {
 
     let size = props.size;
-    let color = "";let textColor = "";
-    if(size & 1) {color = "#303030"; textColor="#f4f4f4"}
+    let color = "";let textColor = "";let youTube,twitter,insta,linkedIn;
+    if(size & 1) {
+        color = "#303030"; textColor="#f4f4f4";
+         youTube = youTubelogoW;
+         twitter=twitterlogoW;
+         insta= instalogoW;
+         linkedIn= linkedInlogoW;
+    }
     else {
         color = "#f4f4f4";
+        youTube = youTubelogoB;
+         twitter=twitterlogoB;
+         insta= instalogoB;
+         linkedIn= linkedInlogoB;
     }
     return (
         <div id="footer-container"  className="flex flex-row" style={{backgroundColor: color}}>
@@ -33,10 +47,10 @@ const Footer = (props) =>
                         <p align="left"  className="text-sm">+91 -361-258162<br/>sgc@iitg.ac.in</p>
                         </div>
                         <div id="social-media-links" className="flex flex-row pt-3  ">
-                        <a href="https://www.youtube.com/@sportsboardiitguwahati"><img  className="h-8 w-8" src={youTubelogo} alt=""/></a>
-                        <a href="https://twitter.com/IITGuwahati"><img  className="h-8 w-8" src={twitterlogo} alt=""/></a>
-                        <a href="https://instagram.com/sports_iit_guwahati?igshid=YmMyMTA2M2Y="><img   className="h-8 w-8" src={instalogo} alt=""/></a>
-                        <a href="https://www.linkedin.com/school/iitg/"><img  className="h-8 w-8" src={linkedInlogo} alt="" /> </a>
+                        <a href="https://www.youtube.com/@sportsboardiitguwahati"><img  className="h-8 w-8" src={youTube} alt=""/></a>
+                        <a href="https://twitter.com/IITGuwahati"><img  className="h-8 w-8" src={twitter} alt=""/></a>
+                        <a href="https://instagram.com/sports_iit_guwahati?igshid=YmMyMTA2M2Y="><img   className="h-8 w-8" src={insta} alt=""/></a>
+                        <a href="https://www.linkedin.com/school/iitg/"><img  className="h-8 w-8" src={linkedIn} alt="" /> </a>
                     </div>
                     </div>
                 
