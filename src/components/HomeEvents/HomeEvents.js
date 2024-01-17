5c99ccf08bc006bc2f6c5bf529c2a292f61a05d6
Arpit Sureka <surekaarpit@gmail.com>
2024-01-17 23:09:55 +0530

    Converted to Cult board

--- src/components/HomeEvents/HomeEvents.js
+++ src/components/HomeEvents/HomeEvents.js
@@ -14,14 +14,14 @@ const HomeEvents = ({data,size}) => {
         size%2?ref.current.className+=" color_blue":ref.current.className+=" color_white"
       },[])
     return (
-            <div className="sports-event" ref={ref} style={{color:textColor}}>
-                <div className="sports-event-detail">
+            <div className="cult-event" ref={ref} style={{color:textColor}}>
+                <div className="cult-event-detail">
                     <Event event={data[0]} id={1}></Event>
                 </div>
-                <div className="sports-event-detail" >
+                <div className="cult-event-detail" >
                     <Event event={data[1]} id={2}></Event>
                 </div>
-                <div className="sports-event-detail" >
+                <div className="cult-event-detail" >
                     <Event event={data[2]} id={3}></Event>
                 </div>
             </div>
